39e9d5bad42a5d0d81e43a0b381483c25743eb5d
michellejanosi <michelle.janosi@gmail.com>
2016-06-12 12:12:50 -0400

    Added solutions to exercise

---
+++ Preparations/my_folder/one.rb
@@ -0,0 +1,3 @@
+# Write a ruby program that outputs the line "this is file one".
+
+puts "this is file one"
---
+++ Preparations/my_folder/two.rb
@@ -0,0 +1,3 @@
+# Write a program that outputs "this is file two".
+
+puts "this is file two"
