b7fe9b6a7dc4d9da2a83cbcbe430e1de2ea5d5f8
michellejanosi <michelle.janosi@gmail.com>
2016-06-28 17:02:43 -0400

    Added solutions to exercises 5 and 6

---
+++ exercises/duplicates.rb
@@ -0,0 +1,5 @@
+# Get rid of duplicates without specifically removing any one value.
+
+array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10]
+
+array.uniq # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
