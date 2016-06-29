6ffad52f07bc8d926d46aeed7b9c3eb7b1b3974b
michellejanosi <michelle.janosi@gmail.com>
2016-06-29 11:46:58 -0400

    Added solution to exercise 8

---
+++ exercises/hashes.rb
@@ -0,0 +1,6 @@
+# Create a Hash using both Ruby syntax styles.
+
+cars = {:Audi => 'A3', :BMW => 'M3', :Lotus => 'Elise', :Tesla => 'Model S'}
+# old way
+cars = {Audi: 'A3', BMW: 'M3', Lotus: 'Elise', Tesla: 'Model S'}
+# new way
