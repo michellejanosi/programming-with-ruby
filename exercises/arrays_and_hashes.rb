0405b8251065693f4cffd6adfefc86d2137a80b7
michellejanosi <michelle.janosi@gmail.com>
2016-06-30 12:49:42 -0400

    Added solution to exercise 10

---
+++ exercises/arrays_and_hashes.rb
@@ -0,0 +1,10 @@
+# Can hash values be arrays? Can you have an array of hashes? (give examples)
+
+# Hash values as arrays? => yes
+hash = {cars: ["Acura", "BMW", "Lotus"], animals: ["cats", "dogs", "birds"],
+  fruit: ["apple", "banana", "kiwi"]}
+
+# Array of hashes? => yes
+array = [{Acura: "A4", BMW: "M3", Lotus: "Elise"}, {cat: "Siamese",
+  dog: "French Bulldog", bird: "parakeet"}, {banana: "yellow", apple: "red",
+    kiwi: "brown"}]
