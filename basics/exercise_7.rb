bacdb42b05335c97678a0008e1e45dbe3321b722
michellejanosi <michelle.janosi@gmail.com>
2016-07-19 08:42:36 -0400

    Updated/corrected exercise 7

---
+++ basics/exercise_7.rb
@@ -0,0 +1,6 @@
+# What does the following error message tell you?
+
+# There is a missing closing bracket
+
+SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+    from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
